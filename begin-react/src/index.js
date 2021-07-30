920d3850f009a53fc2e6e09626ffda918b17bc3d
Dogancan Ulgu <dogancanulgu@gmail.com>
2021-07-31 00:54:41 +0300

    First commit. Begin react

---
+++ begin-react/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+    return <h1>Benim adım Doğancan</h1>;
+}
+
+// class App2 extends React.Component {
+//     render () {
+//         return <h1>Benim adım Doğancan.</h1>;
+//     }
+// }
+
+ReactDOM.render(
+    <App/>,
+    document.getElementById("root")
+);
